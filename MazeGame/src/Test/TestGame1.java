da92e67e84ab21aa67021546a3f8057c321ae153
admin <admin@12b>
2018-11-14 10:55:23 +0700

    Game

---
+++ MazeGame/src/Test/TestGame1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+import java.awt.Color;
+import Model.Sel;
+import Model.Tempat;
+import View.GameFrame;
+import View.TempatPanel;
+/**
+ *
+ * @author admin
+ */
+public class TestGame1 {
+    public static void main(String[] args) {
+        // buat 1 sel
+        Sel sel = new Sel(1,1 ,20,20, '@', Color.RED);
+        // buat 1 tempat dan tambahkan sel ke tempat
+        Tempat tempat = new Tempat();
+        tempat.tambahSel(sel);
+        // Set ukuran tempat
+        Tempat.batasKanan=500;
+        Tempat.batasBawah=300;
+        // buat tempatPanel dan tambahkan tempat ke tempatPanel
+        TempatPanel tempatPanel = new TempatPanel();
+        tempatPanel.setTempat(tempat);
+        // buat gameFrame dan tambahkan tempatPanel ke gameFrame
+        GameFrame game = new GameFrame("My Game",tempatPanel);
+    }
+}
